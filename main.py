32682a8582676806b62e1af1030b853683fbc0cf
elecr0n4ik <andreyshka3@gmail.com>
2023-02-03 12:17:44 +0300

    4

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import prettytable
-from create_update_tables import User
+import sqlite3
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lesson16.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -9,9 +10,24 @@ app.config['JSON_AS_ASCII'] = False
 app.url_map.strict_slashes = False
 db: SQLAlchemy = SQLAlchemy(app)
 
+@app.errorhandler(404)
+def error_404(error):
+    """
+    Представление для ошибки "Страница не найдена"
+    """
+    return "<h1>Страница не найдена</h1>", 404
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    """
+    Представление для ошибки "Внутренняя ошибка сервера"
+    """
+    return "<h1>Сервер не отвечает</h1>", 500
+
+
 @app.route("/users")
 def get_all_users():
-    import sqlite3
 
     result = []
     con = sqlite3.connect("lesson16.db")
@@ -24,12 +40,24 @@ def get_all_users():
 
     return jsonify(users)
 
-# session = db.session()
-# cursor_user = session.execute("SELECT * from `user`").cursor
-# mytable = prettytable.from_db_cursor(cursor_user)
-#
-# if __name__ == '__main__':
-#     print(mytable)
+
+@app.route("/users/<user_id>")
+def get_user_by_id(user_id):
+
+    result = []
+    con = sqlite3.connect("lesson16.db")
+    cur = con.cursor()
+    sqlite_query = f"""SELECT *
+                    FROM user
+                    WHERE id 
+                    LIKE {int(user_id)}
+                    """
+    cur.execute(sqlite_query)
+    user = cur.fetchall()
+    con.close()
+
+    return jsonify(user)
+
 
 if __name__ == '__main__':
     app.run()
